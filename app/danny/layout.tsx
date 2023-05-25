637727b61fa65b10a7180a7ca80b9ee99ed40eb4
dannycurrie <danny.currie42@gmail.com>
2023-05-25 15:28:28 +0100

    clean up dir structure

--- app/danny/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <main className="flex h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl max-h-full items-center justify-between font-mono text-sm lg:flex">
-        {children}
-      </div>
-    </main>
-  )
-}
